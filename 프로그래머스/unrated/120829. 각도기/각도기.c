af9df92fd40cb2c9389580821550ae6fff384c10
SIRYU <siryu2409@gmail.com>
2024-01-11 09:40:33 +0900

    [level unrated] Title: 각도기, Time: 0.01 ms, Memory: 4.39 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120829. 각도기/각도기.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int angle) {
+    if(angle < 90)
+        return 1;
+    else if(angle == 90)
+        return 2;
+    else if(angle == 180)
+        return 4;
+    else
+        return 3;
+}
