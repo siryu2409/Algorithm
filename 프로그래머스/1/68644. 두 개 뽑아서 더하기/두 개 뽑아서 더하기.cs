01d30377d21cce8ca00f433e88fc7d1490662a73
SIRYU <siryu2409@gmail.com>
2024-03-22 05:33:18 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 2.48 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution {
+    public int[] solution(int[] numbers) {
+        List<int> answer = new List<int>();
+        Array.Sort(numbers);
+        
+        for(int i = 0; i < numbers.Length - 1; i++)
+        {
+            for(int j = i+1; j < numbers.Length; j ++)
+            {  
+                int temp = numbers[i] + numbers[j];
+                if(!answer.Contains(temp))
+                    answer.Add(temp);
+            }
+        }
+        answer.Sort();
+        return answer.ToArray();
+    }
+}
